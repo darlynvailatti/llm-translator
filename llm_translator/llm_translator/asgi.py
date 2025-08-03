3aeea74357e5894ff31c3cb0b7cfd98c4db32339
Tathira123 <Tathira.Charoliya@student.Torrens.edu.au>
2025-08-03 10:30:00 +0930

    Update asgi.py

--- llm_translator/llm_translator/asgi.py
+++ llm_translator/llm_translator/asgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "llm_translator.settings")
 
 application = get_asgi_application()
+
+print("ASGI application initialized.")  # Added line

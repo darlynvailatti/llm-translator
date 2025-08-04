46d9cdd88831decd88ecadc9c632d3f9112e470e
SARITA LUNAGARIYA <saritalunagariya2588@gmail.com>
2025-08-04 11:23:27 +0930

    Update asgi.py

--- llm_translator/llm_translator/asgi.py
+++ llm_translator/llm_translator/asgi.py
@@ -10,8 +10,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 
+# Setting the default settings module for Django ASGI
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "llm_translator.settings")
 
 application = get_asgi_application()
 
 print("ASGI application initialized.")  # Added line
+print("Environment setup complete.")    # New harmless debug line
+# Note: This file is safe to modify for logging/debug purposes.
